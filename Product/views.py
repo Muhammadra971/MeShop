62e44a6f494fd718001bd33cb1b89ee05595f99a
Rafi <muhammadrafeek569a@gmail.com>
2025-08-14 02:37:53 +0530

    product error

--- Product/views.py
+++ Product/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
+from . models import Product
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
     
 def product_list(request):
-    return render(request, 'Products.html')
+    product = Product.objects.all()
+    context = {
+        'products': product,
+    }
+    return render(request, 'Products.html', context)
 
 def product_detail(request):
     return render(request, 'detail.html')
